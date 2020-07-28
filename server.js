4673b7fc24188c8963a7d72aadba50ff7dc33360
Diego Cruz <diegodemcruz@gmail.com>
2020-07-28 18:46:24 -0300

    achei brecho back server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 app.use(bodyParser.urlencoded({extend: true}))
 app.use(cors())
